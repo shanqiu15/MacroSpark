8cbf7f33c6ff56ca7da603b11434391a27f6ebe7
shanqiu15 <chenh1987@gmail.com>
2015-05-02 22:41:59 -0700

    Rename Driver and add Spark Context

--- driver.py
+++ driver.py
@@ -1,56 +1,270 @@
-import pickle
-import StringIO
+RDD_ID = 0
 
-import zerorpc
-import cloudpickle
+class RDD(object):
 
-from rdd import *
+    def __init__(self):
 
-# r = TextFile('myfile')
-# m = Map(r, lambda s: s.split())
-# f = Filter(m, lambda a: int(a[1]) > 2)
-# j = Join("old_1", "old_2");
-class Rdd(RDDPartition):
-	def __init__(self, worker_list):
+        #Set a unique id for the RDD
+        global RDD_ID
+        RDD_ID = RDD_ID + 1
+        self.id = RDD_ID
 
 
-class Driver(object):
-    def __init__(self, worker_list):
-        self.worker_list = worker_list
-        self.connections = []
-        for index, worker in enumerate(worker_list):
-            c = zerorpc.Client(timeout=1)
-            c.connect("tcp://" + worker)
-            self.connections.append(c)
-            #c.set_cluster_info(self.worker_list, index)
+    # def collect(self):
+    #     elements = []
+    #     for element in self.get():
+    #         elements.append(element)
+    #     return elements
+
+    # def count(self):
+    #     return len(self.collect())
+
+class TextFile(RDD):
+
+    def __init__(self, filename):
+        super(TextFile, self).__init__()
+        self.filename = filename
+
+        #Store all the operations until now (a generator list)
+        self.lineage = []
+        self.set_lineage()
+
+    def need_repartition(self):
+        return False
+
+    def set_lineage(self):
+        self.lineage.append(self.get())
+
+    #each partition will call yield and seperate the lineage to different stages
+    def get_lineage(self):
+        return self.lineage
+
+    def get(self):
+        yield self
+
+class Map(RDD):
+
+    def __init__(self, parent, func):
+        super(Map, self).__init__()
+        self.parent = parent
+        self.func = func
+
+        #Store all the operations until now (a generator list)
+        self.lineage = []
+        self.set_lineage()
+
+
+    def need_repartition(self):
+        return False
+
+    def set_lineage(self):
+        parent = next(self.parent.get())
+        self.lineage = parent.get_lineage()
+        self.lineage.append(self.get())
+
+    #each partition will call yield and seperate the lineage to different stages
+    def get_lineage(self):
+        return self.lineage
+
+    def get(self):
+        yield self
+
+class Filter(RDD):
     
-    def readFile(self, filePath):
-        for conn in self.connections:
-            conn.readFile(filePath)
+    def __init__(self, parent, func):
+        super(Filter, self).__init__()
+        self.parent = parent
+        self.func = func
 
-    def showPartition(self):
-        for conn in self.connections:
-            conn.showPartition()
+        #Store all the operations until now (a generator list)
+        self.lineage = []
+        self.set_lineage()
 
-    def linage(self):
-        r = yield
-        m = r.rdd_map(lambda s: s.split())
-        f = m.rdd_filter(lambda a: int(a[1]) > 2)
+    def need_repartition(self):
+        return False
 
-# output = StringIO.StringIO()
-# pickler = cloudpickle.CloudPickler(output)
-# pickler.dump(f)
-# # pickler.dump(j)
-# objstr = output.getvalue()
+    def set_lineage(self):
+        parent = next(self.parent.get())
+        self.lineage = parent.get_lineage()
+        self.lineage.append(self.get())
 
-# c = zerorpc.Client()
-# c.connect("tcp://127.0.0.1:4242")
+    #each partition will call yield and seperate the lineage to different stages
+    def get_lineage(self):
+        return self.lineage
 
-# print c.run(objstr)
+    def get(self):
+        yield self
+
+class FlatMap(RDD):
+    def __init__(self, parent):
+        super(FlatMap, self).__init__()
+        self.parent = parent
+        self.func = func
+
+        #Store all the operations until now (a generator list)
+        self.lineage = []
+        self.set_lineage()
+
+    def need_repartition(self):
+        return False
+
+    def set_lineage(self):
+        parent = next(self.parent.get())
+        self.lineage = parent.get_lineage()
+        self.lineage.append(self.get())
+
+    #each partition will call yield and seperate the lineage to different stages
+    def get_lineage(self):
+        return self.lineage
+
+    def get(self):
+        yield self
+
+class ReduceByKey(RDD):
+    def __init__(self, parent, func):
+        super(ReduceByKey, self).__init__()
+        self.parent = parent
+        self.func = func
+
+        #Store all the operations until now (a generator list)
+        self.lineage = []
+        self.set_lineage()
+
+    def need_repartition(self):
+        return False
+
+    def set_lineage(self):
+        parent = next(self.parent.get())
+        self.lineage = parent.get_lineage()
+        self.lineage.append(self.get())
+
+    #each partition will call yield and seperate the lineage to different stages
+    def get_lineage(self):
+        return self.lineage
+
+    def get(self):
+        yield self
+
+class MapValue(RDD):
+    def __init__(self, parent, func):
+        super(MapValue, self).__init__()
+        self.parent = parent
+        self.func =func
+
+        #Store all the operations until now (a generator list)
+        self.lineage = []
+        self.set_lineage()
+
+    def need_repartition(self):
+        return False
+
+    def set_lineage(self):
+        parent = next(self.parent.get())
+        self.lineage = parent.get_lineage()
+        self.lineage.append(self.get())
+
+    #each partition will call yield and seperate the lineage to different stages
+    def get_lineage(self):
+        return self.lineage
+
+    def get(self):
+        yield self
+
+class Join(RDD):
+    def __init__(self, parent, rhd_rdd):
+        super(Join, self).__init__()
+        self.parent = parent
+        self.rhs_rdd = rhs_rdd
+
+        #Store all the operations until now (a generator list)
+        self.lineage = []
+        self.set_lineage()
+
+    def need_repartition(self):
+        return False
+
+    def set_lineage(self):
+        parent = next(self.parent.get())
+        self.lineage = parent.get_lineage()
+        self.lineage.append(self.get())
+
+    #each partition will call yield and seperate the lineage to different stages
+    def get_lineage(self):
+        return self.lineage
+
+    def get(self):
+        yield self
+
+
+class SparkContext():
+    def __init__(self):
+    # def __init__(self, worker_list):
+    #     #setup cluster worker connections
+    #     self.workers  = worker_list
+    #     self.connections = []
+    #     for index, worker in enumerate(worker_list):
+    #         if self.index != index:
+    #             c = zerorpc.Client(timeout=1)
+    #             c.connect("tcp://" + worker)
+    #             self.connections.append(c)
+
+        # map the inputs to the function blocks
+        self.options = { "TextFile"    : self.visitTextFile,
+                         "Map"         : self.visitMap,
+                         "Filter"      : self.visitFilter,
+                         "FlatMap"     : self.visitFlatmap,
+                         "ReduceByKey" : self.visitReduceByKey,
+                         "MapValue"    : self.visitMapValue,
+                         "Join"        : self.visitJoin,
+        }
+
+
+    def visit_lineage(self, rdd):
+        for i in rdd.get_lineage():
+            op = next(i)
+            self.options[op.__class__.__name__]()
+
+    # define the function blocks
+    def visitTextFile(self):
+        print "visit TestFile.\n"
+
+    def visitMap(self):
+        print "visit Map\n"
+
+    def visitFilter(self):
+        print "visit Filter\n"
+
+    def visitFlatmap(self):
+        print "visit FlatMap\n"
+
+    def visitReduceByKey(self):
+        print "visit ReduceByKey\n"
+
+    def visitMapValue(self):
+        print "visit MapValue\n"
+
+    def visitJoin(self):
+        print "visit join\n"
+
 
 
 if __name__ == "__main__":
-    worker_list = ["127.0.0.1:9001", "127.0.0.1:9002"]
-    driver = Driver(worker_list)
-    driver.readFile("myfile")
-    driver.showPartition()
+    # j = Join("old_1", "old_2");
+    # for i in j.get():
+    #     print i
+    # print j.get()
+    r = TextFile('myfile')
+    m = Map(r, lambda s: s.split())
+    f = Filter(m, lambda a: int(a[1]) > 2)
+    mv = MapValue(f, lambda s:s)
+    r = ReduceByKey(mv, lambda x, y: x + y)
+    # print f.collect(), f.count()
+
+    
+    sc = SparkContext()
+    sc.visit_lineage(r)
+    # for i in r.get_lineage():
+    #     op = next(i)
+    #     print op.id
+    #     print op.__class__.__name__
+    #i = r.get_lineage.pop()
