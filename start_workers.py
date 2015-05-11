c878958b5488d65ae1cfa9e1a16ca6d179c5605b
mma5usf <mma5@usfca.edu>
2015-05-10 20:52:35 -0700

    add worker start and stop script

--- start_workers.py
+++ start_workers.py
@@ -5,25 +5,45 @@ __author__ = 'hao'
 python start_workers.py hao@localhost start_workers.sh
 '''
 
+import inspect
 import os
 import os.path
 import subprocess
 import sys
-import gevent
-from gevent.pool import Group
+
+MACRO_SPARK_DIR = "/Users/mingluma/2015Spring/OS2015s/macroSpark/MacroSpark"
+#MACRO_SPARK_DIR = "/Local/Users/hao/Desktop/MacroSpark/worker.py"
 
 class Remote(object):
+    def __init__(self, worker_addr):
+        self.host = worker_addr.split(":")[0]
+        self.port = worker_addr.split(":")[1]
+        self.root_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
-    def __init__(self, host, filename):
-        self.host = host
-        self.filename = filename
-        self.curdir = os.getcwd() #get the current working directory
-        self.remote_file = os.path.join(self.curdir, self.filename)
+    def start(self):
+        subprocess.Popen(['ssh',
+                              self.host,
+                              "python",
+                              MACRO_SPARK_DIR + "/worker.py",
+                              "127.0.0.1:" + self.port])
 
-    def run(self):
-        subprocess.call(['ssh', self.host, "bash", self.remote_file])
+    def stop(self):
+        subprocess.Popen([
+            'ssh',
+            self.host,
+            "ps aux |grep 127.0.0.1:" + self.port + "|xargs kill -9"
+        ])
 
 
 if __name__ == '__main__':
-    remote = Remote(sys.argv[1], sys.argv[2])
-    remote.run()
+    remotes = [
+        Remote("127.0.0.1:9001"),
+        Remote("127.0.0.1:9002"),
+        Remote("127.0.0.1:9003"),
+    ]
+
+    for r in remotes:
+        if sys.argv[1] == "start":
+            r.start()
+        elif sys.argv[1] == "stop":
+            r.stop()
